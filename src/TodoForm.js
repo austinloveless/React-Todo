814ee63ba67f47b8cf785306193f12321a2bbfec
austinloveless <austinloveless5171@gmail.com>
2018-09-04 13:57:41 -0600

    full crud

---
+++ src/TodoForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+  handleSubmit = () => {
+    this.props.addTodo(this.state.inputValue);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}> Add Todo </button>
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
